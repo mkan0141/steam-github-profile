c62d1a2ab9cd8c19789ed9b08a3669dc9a7c7ba8
mkan0141 <kanzume0141@gmail.com>
2023-12-11 23:09:12 +0900

    meow

--- src/app/api/status/route.ts
+++ src/app/api/status/route.ts
@@ -1,48 +1,43 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 import { ZodError, z } from "zod";
 
-import { THEME_LIST, generateSvg } from "@/themes";
+import { THEME_LIST, Theme, generateSvg } from "@/themes";
 import { fetchPlayerSummary, fetchGameDetail } from "@/lib/steam";
 
-const themeValidator = (params: any) => {
-  return z.enum(THEME_LIST).parse(params);
-};
-
-const steamIdValidator = (params: any) => {
-  return z
+const QueryParamsValidator = z.object({
+  steam_id: z
     .string({ invalid_type_error: "'steam_id' is a required query parameter." })
-    .min(1, { message: "Invalid Steam ID. Please check your steam id." })
-    .parse(params);
-};
+    .min(1, { message: "Invalid Steam ID. Please check your steam id." }),
+  theme: z.enum(THEME_LIST).default("default"),
+});
 
-export async function GET(request: NextRequest, response: NextResponse) {
-  const searchParams = request.nextUrl.searchParams;
+export async function GET(request: NextRequest) {
+  const queryParams = request.nextUrl.searchParams;
 
-  // validate query parameters
-  let theme, steamId;
   try {
-    theme = themeValidator(searchParams.get("theme") || "default");
-    steamId = steamIdValidator(searchParams.get("steam_id"));
+    const { steam_id, theme } = QueryParamsValidator.parse(Object.fromEntries(queryParams));
+
+    const svg = await generateSteamCardSvg(steam_id, theme);
+
+    return new Response(svg, { headers: { "Content-Type": "image/svg+xml" } });
   } catch (err) {
     const errorMessage =
       err instanceof ZodError ? `Bad Request. ${err.errors[0].message}` : "Bad Request.";
 
     return new Response(errorMessage, { status: 400 });
   }
+}
 
+const generateSteamCardSvg = async (steamId: string, theme: Theme): Promise<string> => {
   const { gameid: gameId } = await fetchPlayerSummary(steamId);
-  const gameUrl = `https://store.steampowered.com/${gameId ? `app/${gameId}` : ""}`;
 
+  const gameUrl = `https://store.steampowered.com/${gameId ? `app/${gameId}` : ""}`;
   // not playing game
   if (!gameId) {
-    const svg = await generateSvg(theme, gameUrl);
-
-    return new Response(svg, { headers: { "Content-Type": "image/svg+xml" } });
+    return generateSvg(theme, gameUrl);
   }
 
   const gameDetail = await fetchGameDetail(gameId);
 
-  const svg = await generateSvg(theme, gameUrl, gameDetail.name, gameDetail.header_image);
-
-  return new Response(svg, { headers: { "Content-Type": "image/svg+xml" } });
-}
+  return generateSvg(theme, gameUrl, gameDetail.name, gameDetail.header_image);
+};
